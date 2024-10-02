383e923d45382f182f51000dc439823dcbffc1b6
Techit Sumo Thawiang <68545084+techitwinner@users.noreply.github.com>
2024-10-02 20:23:32 +0700

    Changed some SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config'
 
+import tailwind from '@astrojs/tailwind';
+
 export default defineConfig({
-    site: 'https://techit.win',
+  site: 'https://techit.win',
+  integrations: [tailwind()],
 })
