5b71e294064518de79f946469efd3b4f524e993f
techit <techit@dailitation.xyz>
2025-02-17 20:05:29 +0700

    Added support for copyright notice

--- packages/frontend/components/footer.vue
+++ packages/frontend/components/footer.vue
@@ -3,10 +3,10 @@
     <div class="child footer">
       <section class="content-section">
         <h3 v-html="t('footer.title')" style="font-size: 1.5rem;"></h3>
-        <p style="font-size: 12px;">
+        <p>
           {{ t('footer.powered') }} <a class="link" href="https://dailitation.xyz">dailitation.xyz</a><br/>
-          &copy; 2023 - {{ new Date().getFullYear() }} {{ t('me.name') }}, {{ t('footer.copy.rights') }}<br/>
-          <a class="link" href="https://keys.openpgp.org/vks/v1/by-fingerprint/32321602C7B57839F6F800434931394BAB824F2A">{{ t('footer.gpg') }}</a> | <a class="link" href="https://github.com/techitwinner/web">{{ t('footer.sourcecode') }}</a>
+          &copy; 2023 - {{ new Date().getFullYear() }} {{ t('me.name') }}. {{ t('footer.copy.rights') }}<br/>
+          <a class="link" href="https://keys.openpgp.org/vks/v1/by-fingerprint/32321602C7B57839F6F800434931394BAB824F2A">{{ t('footer.gpg') }}</a> | <a class="link" href="https://github.com/techitwinner/web">{{ t('footer.sourcecode') }}</a> | <NuxtLinkLocale class="link" href="/copyright-notice">{{ t('footer.copy.notice') }}</NuxtLinkLocale>
         </p>
       </section>
       <section class="content-section items-end">
