9129aa5ecbd9ce4d8d9fba5e5ebafea828ea8009
Techit Thawiang <techit@dailitation.xyz>
2025-07-27 17:08:00 +0700

    change structurue

--- components/web-footer.vue
+++ components/web-footer.vue
@@ -4,15 +4,17 @@ import WebThemeToggle from './web-theme-toggle.vue';
 </script>
 
 <template>
-    <footer class="article">
-        <section class="web-section">
-            <h3 class="text-3xl md:text-5xl mb-4">Techit's Home /// thawia.ng</h3>
-            <small>
-                Copyright &copy; Techit Thawiang 2025 (2568). All rights reserved.<br>
-                PGP/GPG Key: <code style="font-size: 12px;"><a href="/portal/f/Techit Thawiang_0xE649CED321557334_public.asc">4116 33BE 1B4A 19D4 8D77  9ADE E649 CED3 2155 7334</a></code><br>
-                Powered by <a href="https://dailitation.xyz">dailitation.xyz</a>, <a href="https://github.com/TechitWinner/web">Source Code</a>.
-            </small><br/>
-            <WebThemeToggle/>
-        </section>
+    <footer>
+        <div class="article">
+            <section class="web-section">
+                <h3 class="text-3xl md:text-5xl mb-4">Techit's Home /// thawia.ng</h3>
+                <small>
+                    Copyright &copy; Techit Thawiang 2025 (2568). All rights reserved.<br>
+                    PGP/GPG Key: <code style="font-size: 12px;"><a href="/portal/f/Techit Thawiang_0xE649CED321557334_public.asc">4116 33BE 1B4A 19D4 8D77  9ADE E649 CED3 2155 7334</a></code><br>
+                    Powered by <a href="https://dailitation.xyz">dailitation.xyz</a>, <a href="https://github.com/TechitWinner/web">Source Code</a>.
+                </small><br/>
+                <WebThemeToggle/>
+            </section>
+        </div>
     </footer>
 </template>
