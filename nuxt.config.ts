05903dfab8bd4c1458e1bcb5470239033a188d95
Techit Thawiang <techit@thawia.ng>
2025-10-06 18:28:34 +0700

    append ? to process.env because I feel like it; also remove import for nav.js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,45 +13,45 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      baseUrl: process.env.NUXT_PUBLIC_BASE_URL,
-      siteAuthor: process.env.NUXT_PUBLIC_SITE_AUTHOR,
-      siteName: process.env.NUXT_PUBLIC_SITE_NAME,
-      twitterUsername: process.env.NUXT_PUBLIC_TWITTER_USERNAME,
-      fontUrl: process.env.NUXT_PUBLIC_FONTS_URL
+      baseUrl: process?.env?.NUXT_PUBLIC_BASE_URL,
+      siteAuthor: process?.env?.NUXT_PUBLIC_SITE_AUTHOR,
+      siteName: process?.env?.NUXT_PUBLIC_SITE_NAME,
+      twitterUsername: process?.env?.NUXT_PUBLIC_TWITTER_USERNAME,
+      fontUrl: process?.env?.NUXT_PUBLIC_FONTS_URL
     }
   },
   modules: ['@nuxt/content', '@nuxtjs/sitemap', '@nuxt/image', '@nuxt/eslint', '@nuxtjs/color-mode', '@nuxt/icon'],
   app: {
-    baseURL: process.env.NUXT_PUBLIC_BUILD_BASE_URL,
+    baseURL: process?.env?.NUXT_PUBLIC_BUILD_BASE_URL,
     // pageTransition: { name: 'page', mode: 'out-in' },
     head: {
-      titleTemplate: `%s / ${process.env.NUXT_PUBLIC_SITE_NAME}`,
+      titleTemplate: `%s / ${process?.env?.NUXT_PUBLIC_SITE_NAME}`,
       meta: [
-        { name: 'description', content: `Just another personal website, Created by ${process.env.NUXT_PUBLIC_AUTHOR}` },
+        // { name: 'description', content: `Just another personal website, Created by ${process?.env?.NUXT_PUBLIC_AUTHOR}` }, // Included in pages/index.vue
         { name: 'referrer', content: 'strict-origin-when-cross-origin' },
         { name: 'robots', content: 'index,follow' },
-        { name: 'author', content: process.env.NUXT_PUBLIC_AUTHOR },
-        { name: 'application-name', content: process.env.NUXT_PUBLIC_SITE_NAME },
+        { name: 'author', content: process?.env?.NUXT_PUBLIC_AUTHOR },
+        { name: 'application-name', content: process?.env?.NUXT_PUBLIC_SITE_NAME },
         { name: 'keywords', content: 'techit, techit thawiang, techit, techitwinner, thawiang' },
         { name: 'theme-color', content: '#0066FF' },
         // Open Graph
-        { property: 'og:title', content: `${process.env.NUXT_PUBLIC_SITE_NAME} by ${process.env.NUXT_PUBLIC_AUTHOR}` },
-        { property: 'og:description', content: `Just another personal website, Created by ${process.env.NUXT_PUBLIC_AUTHOR}` },
-        { property: 'og:url', content: process.env.NUXT_PUBLIC_BASE_URL },
-        { property: 'og:site_name', content: process.env.NUXT_PUBLIC_SITE_NAME },
+        { property: 'og:title', content: `${process?.env?.NUXT_PUBLIC_SITE_NAME} by ${process?.env?.NUXT_PUBLIC_AUTHOR}` },
+        { property: 'og:description', content: `Just another personal website, Created by ${process?.env?.NUXT_PUBLIC_AUTHOR}` },
+        { property: 'og:url', content: process?.env?.NUXT_PUBLIC_BASE_URL },
+        { property: 'og:site_name', content: process?.env?.NUXT_PUBLIC_SITE_NAME },
         { property: 'og:type', content: 'website' },
         { property: 'og:locale', content: 'en_US' },
         // Twitter
         { name: 'twitter:creator', content: '@techitwinner' },
         { name: 'twitter:site', content: '@techitwinner' },
         { name: 'twitter:card', content: 'summary' },
-        { name: 'twitter:url', content: process.env.NUXT_PUBLIC_BASE_URL },
+        { name: 'twitter:url', content: process?.env?.NUXT_PUBLIC_BASE_URL },
         { name: 'twitter:image', content: '' },
         // Apple Web App
         { name: 'mobile-web-app-capable', content: 'yes' },
         { name: 'apple-mobile-web-app-capable', content: 'yes' },
         { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
-        { name: 'apple-mobile-web-app-title', content: process.env.NUXT_PUBLIC_AUTHOR }
+        { name: 'apple-mobile-web-app-title', content: process?.env?.NUXT_PUBLIC_AUTHOR }
       ],
       htmlAttrs: {
         lang: 'en',
@@ -59,7 +59,7 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       link: [
-        { rel: 'canonical', href: process.env.NUXT_PUBLIC_BASE_URL },
+        { rel: 'canonical', href: process?.env?.NUXT_PUBLIC_BASE_URL },
         { rel: 'icon', type: 'image/vnd.microsoft.icon', href: '/favicon.ico' },
         // TYPEFACES
         { rel: 'preconnect', href: 'https://fonts.thawiang.com/' },
@@ -69,15 +69,13 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href: 'https://fonts.thawiang.com/noto-serif-thai/noto-serif-thai.css' }
       ],
       script: [
-        // { src: '/js/ripple.js', type: 'text/javascript', defer: true },
-        { src: '/js/nav.js', type: 'text/javascript', defer: true },
         {
           type: 'application/ld+json',
           innerHTML: JSON.stringify({
             "@context": "https://schema.org",
             "@type": "WebSite",
-            "name": process.env.NUXT_PUBLIC_SITE_NAME,
-            "url": process.env.NUXT_PUBLIC_BASE_URL
+            "name": process?.env?.NUXT_PUBLIC_SITE_NAME,
+            "url": process?.env?.NUXT_PUBLIC_BASE_URL
           })
         }
       ]
